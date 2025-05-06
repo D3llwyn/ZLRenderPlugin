550c8d953c8df2368dfb0413cc489fb8a9d10728
Wendell Matthews <wendellanibal2003@gmail.com>
2025-05-06 14:46:40 -0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,6 +19,8 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "ZalithRendererPlugin"
+rootProject.name = "ZLRenderPlugin"
 include(":app")
+include(":ltw")
+project(":ltw").projectDir = file("libs/LTW")
  
