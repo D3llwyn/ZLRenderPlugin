818f27351bcaa09cab2527e01573fcacff0c7705
Wendell Matthews <wendellanibal2003@gmail.com>
2025-05-07 09:31:13 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     compileSdk = 34
 
     signingConfigs {
-        create("release") {
+        create("customDebug") {
             keyAlias = "key0"
             keyPassword = "ZALITH_RENDERER_PLUGIN"
             storeFile = file("plugin-key.jks")
@@ -27,29 +27,21 @@ android {
     }
 
     buildTypes {
-        release {
-            isMinifyEnabled = false
+        debug {
+            isDebuggable = true
+            signingConfig = signingConfigs.getByName("customDebug")
+              }
         }
         configureEach {
-            //应用名
-            //app name
             resValue("string","app_name","Test Renderer")
-            //包名后缀
-            //package name Suffix
             applicationIdSuffix = ".testrender"
 
-            //渲染器在启动器内显示的名称
-            //The name displayed by the renderer in the launcher
             manifestPlaceholders["des"] = "test render"
-            //渲染器的具体定义 格式为 名称:渲染器库名:EGL库名 例如 Some Renderer:libSome_renderer.so:libSome_renderer.so
             //The specific definition format of a renderer is ${name}:${renderer library name}:${EGL library name}, for example: Some Renderer:libSome_renderer.so:libSome_renderer.so
-            manifestPlaceholders["renderer"] = ""
+            manifestPlaceholders["renderer"] = "${name}"
 
-            //特殊Env
             //Special Env
-            //DLOPEN=libxxx.so 用于加载额外库文件
             //DLOPEN=libxxx.so used to load external library
-            //如果有多个库,可以使用","隔开,例如  DLOPEN=libxxx.so,libyyy.so
             //If there are multiple libraries, you can use "," to separate them, for example  DLOPEN=libxxx.so,libyyy.so
             manifestPlaceholders["pojavEnv"] = mutableMapOf<String,String>().apply {
                 //put("POJAV_RENDERER", "renderer_id")
@@ -61,20 +53,6 @@ android {
                 }
                 env.dropLast(1)
             }
-
-            //FCL可以额外使用Boat启动方式，但ZalithLauncher只能使用Pojav这一种启动方式，所以移除了boatEnv环境变量的配置项，因此，这个渲染器插件就不能为FCL提供支持！
-            //FCL can additionally use the Boat launch method, but ZalithLauncher can only use the Pojav launch method.
-            //As a result, the boatEnv environment variable configuration option was removed. Therefore, this renderer plugin cannot provide support for FCL!
-
-            //manifestPlaceholders["boatEnv"] = mutableMapOf<String,String>().apply {
-
-            //}.run {
-            //    var env = ""
-            //    forEach { (key, value) ->
-            //        env += "$key=$value:"
-            //    }
-            //    env.dropLast(1)
-            //}
         }
     }
     compileOptions {
